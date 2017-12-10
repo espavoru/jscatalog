2e4dbc5443ae65fb2a83517b5db3bf08057e9dcd
espavoru <espavoru@gmail.com>
2017-12-10 15:24:17 +0200

    If else operators

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,3 +1,12 @@
-// $( document ).ready(function() {
+$(document).ready(function () {
+  var message,
+      login = prompt('Your name?', '');
 
-// });
+  message = (login == 'John') ? 'Hello John' :
+          (login == 'CEO') ? 'Whats up!' :
+          (login == '') ? 'No login' :
+          'I dont know you bro';
+
+  console.log(message);
+  alert(message);
+});
