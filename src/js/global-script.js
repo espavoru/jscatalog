e36fd7e4cf7dc9174ef48751f7b636fb4460b6ad
espavoru <espavoru@gmail.com>
2017-12-11 11:40:20 +0200

    Перетворення даних

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
-  var message,
-      login = prompt('Your name?', '');
+  // Перетворення данних
 
-  message = (login == 'John') ? 'Hello John' :
-          (login == 'CEO') ? 'Whats up!' :
-          (login == '') ? 'No login' :
-          'I dont know you bro';
+  var task1 = '' + 1 + 0, // 10
+      task2 = '' - 1 + 0, // -1
+      task3 = true + false, // 1
+      task4 = 6 / '3', // 2
+      task5 = '2' * '3', // 6
+      task6 = 4 + 5 + 'px' // 6
+  ;
 
-  console.log(message);
-  alert(message);
+  console.log('task1 ' + task1);
+  console.log('task2 ' + task2);
+  console.log('task3 ' + task3);
+  console.log('task4 ' + task4);
+  console.log('task5 ' + task5);
+  console.log('task6 ' + task6);
 });
