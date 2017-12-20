a8e8e57361c2d7a529ea3c7cc08738f6ca3ac1b7
espavoru <espavoru@gmail.com>
2017-12-20 17:19:37 +0200

    Object methods, this

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,123 +1,62 @@
 $(document).ready(function () {
   /*
-   * Closures | замикання
+   * Object methods
+   *
+   * this
    */
-
-  /**
-   * Get sum elements
-   * @param {number} a
-   * @returns {Function}
+  ;/**
+   * Calculator
    */
-  function getSumFunc(a) {
-    return function (b) {
-      alert(a + b);
-    };
-  }
-
-//  getSumFunc(2)(30);
-//  getSumFunc(5)(11);
-
-  /**
-   * Make string buffer
-   */
-  function makeBuffer() {
-    var text = '';
-
-    function buffer(piece) {
-      if (arguments.length == 0) { // вызов без аргументов
-        return text;
+  function makeCalculator() {
+    /* @type Object */
+    var calculator = {
+      read: function () {
+        this.numA = +prompt('Enter first number', 0);
+        this.numB = +prompt('Enter first number', 0);
+      },
+      'sum numbers': function () {
+        return this.numA + this.numB;
       }
-      text += piece;
-    }
-    ;
-
-    buffer.clear = function () {
-      text = "";
     };
 
-    return buffer;
-  }
-  ;
-
-  var buffer = makeBuffer();
-  /*
-   buffer('Hello');
-   buffer('World!');
-   alert(buffer());
-
-   buffer.clear();
-   alert(buffer());
-   */
-
-  var users = [{
-      name: 'John',
-      surname: 'Black',
-      age: 20
-    }, {
-      name: 'Peter',
-      surname: 'White',
-      age: 25
-    }, {
-      name: 'Kate',
-      surname: 'Smith',
-      age: 18
-    }
-  ];
-
-  /**
-   * Make sort by field
-   * @param {string} field
-   * @returns {function}
-   */
-  function byField(field) {
-    return function(a, b) {
-      return a[field] < b[field] ? 1 : -1;
+    calculator.mul = function () {
+      return this.numA * this.numB;
     };
+
+    calculator.read();
+
+    alert('calc.numA is ' + calculator['numA']);
+    alert('calc.numB is ' + calculator.numB);
+
+    alert('Sum numbers is ' + calculator['sum numbers']());
+    alert('Multiple numbers is ' + calculator.mul());
   }
+
+//  makeCalculator();
+
 /*
-  users.sort(byField('name'));
-  users.forEach(function(user) {
-    alert(user.name);
-  });
-
-  users.sort(byField('age'));
-  users.forEach(function(user) {
-    alert(user.name);
-  });
-  */
-
-
-  function filter(arr, func) {
-    var result = [];
-
-    for (var i = 0; i < arr.length; i++) {
-      var val = arr[i];
-
-      if (func(val)) {
-        result.push(val);
+ * Chaining
+ */
+  function makeChain() {
+    /* @type Object */
+    var ladder = {
+      step: 0,
+      up: function() {
+        this.step++;
+        return this;
+      },
+      down: function() {
+        this.step--;
+        return this;
+      },
+      showStep: function() {
+        alert('Step is ' + this.step);
+        return this;
       }
-    }
-
-    return result;
-  }
-
-  function inBetween(a, b) {
-    return function(x) {
-      return x >= a && x <= b;
     };
+
+    ladder.up().up().down().up().showStep(); // 2
   }
 
-  function inArray(arr) {
-    return function(x) {
-      return arr.indexOf(x) != -1;
-    };
-  }
-
-  var arr = [1, 2, 3, 4, 5, 6, 7, 8];
-//  alert(filter(arr, function(a){
-//    return a % 2 == 0;
-//  }));
-
-//  alert(filter(arr, inBetween(3, 6)));
-  alert(filter(arr, inArray([1, 2, 10])));
+//  makeChain();
 });
