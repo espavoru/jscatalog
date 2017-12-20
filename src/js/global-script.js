0932a777cf9959d1086008de041931c6fff3b049
espavoru <espavoru@gmail.com>
2017-12-20 19:20:52 +0200

    Constructor new

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,62 +1,98 @@
-$(document).ready(function () {
+$(document).ready(function() {
   /*
-   * Object methods
-   *
-   * this
+   * Functions constructors new
    */
-  ;/**
-   * Calculator
+  /**
+   * Calculators constructor
+   * @returns {global-scriptL#1.Calculator}
    */
-  function makeCalculator() {
-    /* @type Object */
-    var calculator = {
-      read: function () {
-        this.numA = +prompt('Enter first number', 0);
-        this.numB = +prompt('Enter first number', 0);
-      },
-      'sum numbers': function () {
-        return this.numA + this.numB;
-      }
+  function Calculator() {
+    this.read = function() {
+      this.numA = +prompt('What is A number?', 0);
+      this.numB = +prompt('What is B number?', 0);
     };
-
-    calculator.mul = function () {
+    this.sum = function() {
+      return this.numA + this.numB;
+    };
+    this.mul = function() {
       return this.numA * this.numB;
     };
-
-    calculator.read();
-
-    alert('calc.numA is ' + calculator['numA']);
-    alert('calc.numB is ' + calculator.numB);
-
-    alert('Sum numbers is ' + calculator['sum numbers']());
-    alert('Multiple numbers is ' + calculator.mul());
   }
 
-//  makeCalculator();
+  /* @type Object */
+  var myCalc = new Calculator();
 
-/*
- * Chaining
- */
-  function makeChain() {
-    /* @type Object */
-    var ladder = {
-      step: 0,
-      up: function() {
-        this.step++;
-        return this;
+  /*
+   myCalc.read();
+   alert('The sum result is ' + myCalc.sum());
+   alert('The multiplying result is ' + myCalc.mul());
+   */
+
+  /**
+   * Create objects what sum prompt values
+   * @param {number} startingValue
+   * @returns {global-scriptL#1.Accumulator}
+   */
+  function Accumulator(startingValue) {
+    this.value = startingValue;
+    this.read = function() {
+      this.value += +prompt('What is your number?', 0);
+    };
+  }
+
+  var accumulator = new Accumulator(1);
+  /*
+   accumulator.read();
+   accumulator.read();
+   alert('Your sum is ' + accumulator.value);
+   */
+
+  function CalculatorStr() {
+    var methods = {
+      '+': function(a, b) {
+        return a + b;
       },
-      down: function() {
-        this.step--;
-        return this;
-      },
-      showStep: function() {
-        alert('Step is ' + this.step);
-        return this;
+      '-': function(a, b) {
+        return a - b;
       }
     };
 
-    ladder.up().up().down().up().showStep(); // 2
+    this.calculate = function(str) {
+      /* @type Array */
+      var split = str.split(' '),
+          /* @type number */
+          a = +split[0],
+          /* @type string */
+          op = split[1],
+          /* @type number */
+          b = +split[2];
+
+      if (!methods[op] || isNaN(a) || isNaN(b)) {
+        return 'NaN';
+      }
+
+      return methods[op](a, b);
+    };
+
+    this.addMethod = function(name, func) {
+      methods[name] = func;
+    };
   }
 
-//  makeChain();
+  /* @type Object */
+  var calc = new CalculatorStr();
+
+  calc.addMethod('*', function(a, b) {
+    return a * b;
+  });
+  calc.addMethod('/', function(a, b) {
+    return a / b;
+  });
+  calc.addMethod('**', function(a, b) {
+    return Math.pow(a, b);
+  });
+
+  var result = 'Your result is ' + calc.calculate('3 / 7');
+
+  alert(result);
 });
