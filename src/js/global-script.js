0f83f6ebe000ac5d7ff336029e038b881b59a6eb
espavoru <espavoru@gmail.com>
2017-12-26 08:49:51 +0200

    Basic DOM node properties

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,101 +1,24 @@
 $(document).ready(function() {
   /**
-   * DOM searching elements
+   * Basic dom node properties
    */
-  /**
-   * Get DOM elements by tag names
-   * @param {string} elem
-   */
-  function getElements(elem) {
+//  console.dir(document.body);
+//  console.log(document.body);
 
-    let table = document.getElementById('age-table');
-    let elements = table.getElementsByTagName(elem);
+  let elem = document.querySelectorAll('.animals');
 
-    for (var i = 0; i < elements.length; i++) {
-      let oneElem = elements[i];
-      alert(oneElem.value + ': ' + oneElem.checked);
-    }
-  }
-//  getElements('input');
+//  console.log(elem[0].innerHTML = '<b>Not valid code');
 
-  /**
-   * Get elements by class
-   * @param {strig} className
-   */
-  function getElemByClass(className) {
-    "use strict";
-    let parent = document.getElementsByClassName('mainTable')[0];
-    let elementsArr = parent.getElementsByClassName(className);
+  elem = document.querySelector('[name=search]');
+//  console.dir(elem.outerHTML);
 
-    for (var i = 0; i < elementsArr.length; i++) {
-      let oneElem = elementsArr[i];
-      alert(`Elem ${i + 1} ` + oneElem.innerText);
-    }
-  }
+  // заменяем outerHTML на <p>...</p>
+//  elem.outerHTML = '<p>Some new content</p>';
+  // ... но содержимое div.outerHTML осталось тем же, несмотря
+  // на "перезапись"
+//  console.dir(elem.outerHTML);
+  elem = document.querySelector('[name=search]');
 
-//  getElemByClass('mainTable__cell');
-
-  /**
-   * Get all elements by css selector
-   * @param {string} cssSelector
-   */
-  function getQuerySelAll(cssSelector) {
-    "use strict";
-
-    let elementArr = document.querySelectorAll(cssSelector);
-
-    for (let i = 0; i < elementArr.length; i++) {
-      let oneElem = elementArr[i];
-      alert(oneElem.innerText);
-    }
-  }
-
-//  getQuerySelAll('ul > li');
-
-
-  /**
-   * Get elements in diff ways
-   */
-  function getSomeEl() {
-    let labels = document.querySelectorAll('#ageTable label');
-    let firstCell = document.querySelectorAll('#ageTable tr:first-child td:first-child');
-    let secondForm = document.querySelectorAll('form:nth-of-type(2)');
-    let formSearch = document.getElementsByName('search')[0];
-    let inputInSearch = document.querySelectorAll('form[name$=search] input');
-    let infoEl = document.querySelector('[name="info[0]"]');
-    let infoInForm = document.querySelector('form[name="search-person"] [name="info[0]"]');
-    let lis = document.getElementsByTagName();
-
-    for (var i = 0; i < inputInSearch.length; i++) {
-      let elem = inputInSearch[i];
-//      alert(elem.outerHTML);
-    }
-
-//    alert(firstCell[0].outerHTML);
-//    alert(secondForm[0].outerHTML);
-//    alert(formSearch.outerHTML);
-//    alert(inputInSearch.outerHTML);
-//    alert(infoEl.outerHTML);
-//    alert(infoInForm.outerHTML);
-  }
-
-//  getSomeEl();
-
-  function getListItems() {
-      let lis = document.querySelectorAll('.animals li');
-      for (var i = 0; i < lis.length; i++) {
-
-        let title = lis[i].firstChild.data;
-
-        title = title.trim();
-
-        let childCount = +lis[i].getElementsByTagName('li').length;
-
-        childCount ? alert(title + ': ' + childCount) :
-          alert('- ' + title);
-      }
-    };
-
-    getListItems();
-
+//  elem.children[0].hidden = true;
+  console.dir(elem.outerHTML);
 });
