6bd70e6e96c99b7d770209093dc59c6c7dba09dc
espavoru <espavoru@gmail.com>
2017-12-27 10:24:49 +0200

    Modifying document

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,47 +1,128 @@
 $(document).ready(function() {
   /**
-   * Attributes and custom properties
-   */
-  let elem = document.querySelectorAll('[data-custom-search="site-search"]')[0];
-  let elemData = elem.dataset;
-  /*
-   console.dir('Your element is: \n' +
-   elem.outerHTML);
-   console.log('\n\You looking for attribute: \n');
-   console.log(elemData);
-   console.dir('\n\Attribute value is: \n' + elemData.customSearch);
+   * Modifying document
    */
 
-  function setClass() {
-    let css = 'a[href*="://"]:not([href*="http://internal.com"])';
-    let links = document.querySelectorAll(css);
-
-    for (var i = 0; i < links.length; i++) {
-      links[i].classList.add('external');
-    }
-  }
-
-//  setClass();
-
-/*
- * Add custom class to elements
- */
-  function setLinksBg() {
+  function makeElem() {
     "use strict";
 
-    let links = document.querySelectorAll('a');
+    let main = document.querySelector('main');
 
-    for (let i = 0; i < links.length; i++) {
-      let a = links[i];
-      let href = a.getAttribute('href');
+    let div = document.createElement('div');
+    div.classList.add('alert', 'alert--success');
+    div.innerHTML = '<strong>Hello!</strong> BroWorld';
 
-      if (!href) continue; // немає атрибуту
-      if (href.indexOf('://') == -1) continue; // без протоколу
-      if (href.indexOf('http://internal.com') === 0) continue; // внутрішня
+//    main.insertBefore(div, main.querySelector('hr'));
+//    main.querySelector('[name=search]').insertBefore(
+//        div, main.querySelector('[type=submit]'));
+    main.querySelector('[name="search"]').insertBefore(
+        div, main.querySelector('label').nextSibling);
 
-      a.classList.add('external');
+    let div2 = div.cloneNode(true);
+    div2.querySelector('strong').innerHTML = 'Wats up!';
+    div2.classList.add('alert--warning');
+    div2.classList.remove('alert--success');
+    div.parentNode.insertBefore(div2, div.nextSibling);
+
+    makeElem.remove = function() {
+      setTimeout(function() {
+        div.parentNode.removeChild(div2);
+      }, 2000);
+    };
+  }
+
+//  makeElem();
+//  makeElem.remove();
+
+  function makeTextNode() {
+    "use strict";
+
+    let div = document.createElement('div');
+    let parentNode = document.querySelector('main');
+    div.classList.add('alert', 'alert--danger');
+    parentNode.appendChild(div);
+
+    let message = prompt('Enter your message', '<b>Some</b> html cntent');
+
+    // вставится именно как текст, без HTML-обработки
+    div.appendChild(document.createTextNode(message));
+  }
+
+//  makeTextNode();
+
+  /*
+   * Polyfill for remove elem
+   */
+  (function() {
+    if (!Element.prototype.remove) {
+      Element.prototype.remove = function remove() {
+        if (this.parentNode) {
+          this.parentNode.removeChild(this);
+        }
+      };
+    }
+  })();
+
+  elem = document.querySelector('[name=search]');
+//  elem.remove();
+
+  /*
+   * Insert element after
+   */
+  let insertAfter = function(elem, refElem) {
+    return refElem.parentNode.
+        insertBefore(elem, refElem.nextSibling);
+  };
+
+  let refElem = document.querySelector('hr');
+  elem = document.createElement('div');
+  elem.textContent = 'Donec viverra interdum turpis, a auctor' +
+      'nunc aliquam ac. Phasellus iaculis ex in justo eleifend' +
+      'volutpat. Vivamus cursus ex metus, a mollis massa sodales id.';
+  elem.classList.add('alert', 'alert--success');
+
+//  insertAfter(elem, refElem);
+
+/**
+ * Remoove all childrens
+ * @param {string} elem
+ */
+  function removeChildren(elem) {
+    try {
+      elem.innerHTML = '';
+    } catch (e) {
+      while (elem.firstChild) {
+        elem.removeChild(elem.firstChild);
+      }
     }
   }
 
-  setLinksBg();
+  elem = document.getElementById('ageTable');
+//  removeChildren(elem);
+
+/**
+ * Create list with items
+ */
+  function createList() {
+    "use strict";
+
+    let ul = document.createElement('ul');
+    let main = document.querySelector('main');
+
+    main.appendChild(ul);
+
+    while (true) {
+      let userData = prompt('Enter your text for list item', '');
+
+      if (!userData) {
+        break;
+      }
+
+      let li = document.createElement('li');
+      li.appendChild(document.createTextNode(userData));
+      ul.insertBefore(li, null);
+    }
+  }
+
+  createList();
 });
