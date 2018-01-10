2bdc900103b2c5b77f66f1d639a5256463f5439c
espavoru <espavoru@gmail.com>
2018-01-10 20:54:02 +0200

    Simple calculator with using 'this'

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-  
+  var calculator = {
+
+    read: function() {
+      this.a = +prompt('Enter "a" value', '0');
+      this.b = +prompt('Enter "b" value', '0');
+    },
+
+    sum: function() {
+      return this.a + this.b;
+    },
+
+    mul: function() {
+      return this.a * this.b;
+    }
+  };
+
+  calculator.read();
+  alert('The sum of numbers: \n' + calculator.sum());
+  alert('The multiplication of numbers: \n' + calculator.mul());
 });
