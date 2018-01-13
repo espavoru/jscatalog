72a5e0847c323e4b289335fb14472815e246ecbf
espavoru <espavoru@gmail.com>
2018-01-13 12:55:58 +0200

    Searching-elements-dom

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -26,5 +26,73 @@ $(document).ready(function() {
 
   let gridForEach = new GridForEach();
 //  gridForEach.run();
-  gridForEach.runForEach();
+//  gridForEach.runForEach();
+
+  /*
+   * Navigation with child
+   */
+  function Children(_class) {
+    "use strict";
+    let elems = document.querySelector(_class);
+
+    this.run = function() {
+      let result = elems.parentNode.children[1].lastElementChild;
+
+      result = !elems.firstChild ? 'Empty' : ('Has some children \n' +
+          'First child \n');
+
+      console.log(elems);
+      console.log(result);
+      return this;
+    };
+  }
+
+  let children = new Children('.grid');
+//  children.run();
+
+  /*
+   *
+   */
+  function GetElementsBy(cls) {
+    let elem = document.querySelectorAll(cls),
+        elems = document.querySelectorAll(cls),
+        childs;
+
+
+
+    this.run = function() {
+      childs = elem.getElementsByTagName('*');
+      console.dir(childs);
+
+      for (let i = 0; i < childs.length; i++) {
+        childs[i].addEventListener('click', function(e) {
+          console.log(this);
+          this.style.background = '#333';
+        });
+      }
+    };
+
+    this.runArr = function() {
+      console.log(elems);
+
+      for (let i = 0; i < elems.length; i++) {
+        elems[i].addEventListener('click', function() {
+          this.classList.toggle('grid__item--active');
+
+          if (this.innerHTML) {
+            setTimeout(function() {
+              elem[i].innerHTML = '';
+            }, 700);
+          } else {
+            this.innerHTML = 'div ' + (i + 1);
+          }
+        });
+      }
+    };
+  }
+
+  let getElementsBy = new GetElementsBy('.grid *');
+//  getElementsBy.run();
+  getElementsBy.runArr();
+
 });
