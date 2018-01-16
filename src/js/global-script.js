c9294da2a2114731ee8a3a8c447e0aaae60c5b5e
espavoru <espavoru@gmail.com>
2018-01-16 09:49:35 +0200

    A bit advanced popups

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -1,42 +1,87 @@
 $(document).ready(function() {
   "use strict";
 
-  function Alert(options) {
-    let classes = options.classes || 'alert',
-        content = options.content || '<b>Hello</b> World!',
-        place = document.querySelector(options.place) || document.body.children[0];
+  function MyPopup(options) {
+    let closeBtn = options.closeBtn || true,
+        width = options.width || '60%',
+        timerOff = options.timerOff || false,
+        timerOn = options.timerOn || false,
+        content = options.content || 'Hello World',
+        addCss = options.addCss;
 
-    this.newElem = document.createElement('div');
-    this.newElem.className = classes;
-    this.newElem.innerHTML = content;
 
-    function insertTo() {
-      place.parentNode.insertBefore(this.newElem, place);
+    let popupOverlay = document.createElement('div');
+    popupOverlay.className = 'popup';
+    document.body.insertBefore(popupOverlay, null);
+
+    let popupContent = document.createElement('div');
+    popupContent.className = 'popup__content';
+    popupContent.innerHTML = content;
+    popupOverlay.insertBefore(popupContent, null);
+
+    document.body.addEventListener('click', callback);
+
+    function callback(e) {
+      let target = e.target;
+
+      if (target.classList.contains('popup')) {
+        remove(300);
+//          target.remove();
+//        target.parentNode.removeChild(target);
+//        popupOverlay.classList.toggle('popup--hidden');;
+      }
+
+      function remove(t) {
+        target.classList.add('popup--hidden');
+        setTimeout(function() {
+          target.remove();
+        }, t);
+      }
     }
 
-    this.run = function() {
-      this.newElem.style.opacity = 1;
-      insertTo.apply(this);
-    };
-
-    this.stop = function() {
-      this.newElem.style.opacity = 0;
-//      this.newElem.parentNode.removeChild(this.newElem);
-    };
-
-    return this;
   }
 
-  let optionsW = {
-    classes: 'alert alert--warning',
-    content: '<b>Donec viverra</b> interdum turpis, a auctor nunc aliquam ac. Phasellus iaculis ex in justo eleifend volutpat. Vivamus cursus ex metus, a mollis massa sodales id.',
-    place: '.grid'
-  };
 
-//  let mywarning = new Alert(optionsW);
-
+  function menu() {
+    var elem = document.querySelector('.menu .btn__checkout');
+    var options = {
+    };
+    var popupOverlay = document.querySelector('.popup');
 
 
+    document.body.addEventListener('click', callback);
+
+    function callback(e) {
+      e.preventDefault();
+
+      var target = e.target,
+          dataPopup = target.dataset.popup;
+
+      if (dataPopup) {
+        menu[dataPopup]();
+      }
+    }
+
+    menu.checkout = function() {
+      options.content = '<p class="popup__head">Checkout</p>' +
+          '<p class="popup__body">Donec viverra interdum turpis, a auctor nunc aliquam ac. Phasellus iaculis ex in justo eleifend volutpat. Vivamus cursus ex metus, a mollis massa sodales id.</p>';
+      var popup = new MyPopup(options);
+    };
+
+    menu.delivery = function() {
+      options.content = '<p class="popup__head">Delivery</p>' +
+          '<p class="popup__body">Donec viverra interdum turpis, a auctor nunc aliquam ac. Phasellus iaculis ex in justo eleifend volutpat. Vivamus cursus ex metus, a mollis massa sodales id.</p>';
+      var popup = new MyPopup(options);
+    };
+
+    menu.guarantee = function() {
+      options.content = '<p class="popup__head">Guarantee</p>' +
+          '<p class="popup__body">Phasellus risus metus, vulputate scelerisque arcu quis, faucibus volutpat risus. Praesent quis libero blandit, scelerisque magna fringilla, tristique dui. </p>';
+      var popup = new MyPopup(options);
+    };
+  }
+
+  menu();
 
 
   /**
@@ -46,45 +91,53 @@ $(document).ready(function() {
   function Category() {
     let elems = document.querySelectorAll('.grid__item'),
         elemTotal = document.querySelector('.grid__total'),
-        totalSum = 0;
+        totalSum = 0,
+        self = this;
 
-    let optionsA = {
-      classes: 'alert alert--success alert--fixed',
-      content: '<b>Voluptas</b> sit aspernatur aut odit aut' +
-          'fugit, sed quia consequuntur magni dolores eos',
-      place: '.grid__total'
+    let options = {
+      content: 'Hello World!',
+      closeBtn: true,
+      width: '60%',
+      timerOff: false,
+      timerOn: false,
+      addCss: ''
+    };
+    let popupOverlay = document.querySelector('.popup');
+
+    elemTotal.innerHTML = totalSum + ' USD';
+
+    this.first = function() {
+      options.content = 'Lorem Ipsum';
+//      let popup1 = new MyPopup(options);
     };
 
-    let  myalert = new Alert(optionsA);
+    this.start = function() {
+      document.body.addEventListener('click', callback);
+    };
 
-    this.run = function() {
+    function callback(e) {
+      let target = e.target,
+          price = +target.dataset.price;
+
+      if (!target.classList.contains('grid__item'))
+        return;
+
+      target.classList.contains('grid__item--active') ?
+          totalSum -= price : totalSum += price;
+
+      target.classList.toggle('grid__item--active');
       elemTotal.innerHTML = totalSum + ' USD';
 
+      let dataPopup = target.getAttribute('data-popup');
+//          popupOverlay = document.querySelector('.popup');
 
-
-      for (let i = 0; i < elems.length; i++) {
-        elems[i].addEventListener('click', function() {
-          let price = +this.dataset.price;
-
-          this.classList.contains('grid__item--active') ?
-              totalSum -= price : totalSum += price;
-
-          this.classList.toggle('grid__item--active');
-          elemTotal.innerHTML = totalSum + ' USD';
-
-          if (this.classList.contains('grid__item--active')) {
-            myalert.run();
-            setTimeout(function() {
-              myalert.stop();
-            }, 1000);
-          } else {
-            myalert.stop();
-          }
-        });
+      if (dataPopup) {
+//        self[dataPopup]();
+//        popupOverlay.classList.toggle('popup--hidden');
       }
-    };
+    }
   }
 
   let category = new Category();
-  category.run();
+  category.start();
 });
